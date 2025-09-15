070f090d1b62b159454370b5ee334c7e88545049
sdhz151 <yanghui140@gmail.com>
2025-09-15 10:09:00 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,20 +8,18 @@ else
     exit 1    
 fi
 
-apt update && apt upgrade -y && apt autoremove -y
-
 if [[ -f /usr/bin/vlmcsd ]]; then
     echo -e "更新vlmcsd"
     systemctl stop vlmcsd
-    wget --no-check-certificate -O /usr/bin/vlmcsd https://raw.githubusercontent.com/ActiveIce/vlmcsd/master/vlmcsd
+    wget --no-check-certificate -O /usr/bin/vlmcsd https://raw.githubusercontent.com//sdhz151/vlmcsd/master/vlmcsd
     systemctl start vlmcsd
     echo -e "vlmcsd更新成功"
     exit 0
 fi
 
 echo -e "开始安装vlmcsd"
-wget --no-check-certificate -O /usr/bin/vlmcsd https://raw.githubusercontent.com/ActiveIce/vlmcsd/master/vlmcsd
-wget --no-check-certificate -O /etc/systemd/system/vlmcsd.service https://raw.githubusercontent.com/ActiveIce/vlmcsd/master/vlmcsd.service
+wget --no-check-certificate -O /usr/bin/vlmcsd https://raw.githubusercontent.com//sdhz151/vlmcsd/master/vlmcsd
+wget --no-check-certificate -O /etc/systemd/system/vlmcsd.service https://raw.githubusercontent.com//sdhz151/vlmcsd/master/vlmcsd.service
 chmod +x /usr/bin/vlmcsd
 systemctl start vlmcsd
 systemctl enable vlmcsd
